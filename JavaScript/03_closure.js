9bada43285c2ebf2fd328b01b3693901f2765566
Aadesh Thakur <aadeshkbt@gmail.com>
2025-01-05 00:47:21 +0530

    closure in js

---
+++ JavaScript/03_closure.js
@@ -0,0 +1,49 @@
+//function along with it's lexical scope bundles together forms a closure
+//like calling a function within a function
+function x() {
+    var a = 10;
+    function y() {
+        console.log(a);
+    }
+    y();
+}
+
+//x();
+
+
+//set timeout
+function z() {
+    var i = 1;
+    setTimeout(function() {
+        console.log(i);
+    }, 3000);
+    console.log("printed before 1");
+} 
+
+//z();
+
+//example 2
+function z2() {
+    var i = 1;
+    for(var i=0; i<5; i++) {
+        setTimeout(function() {
+            console.log(i);
+        }, i*1000);
+    }
+    console.log("printed before 1");
+} 
+
+//z2();
+
+//example 3
+function z3() {
+    var i = 1;
+    for(let i=0; i<5; i++) {
+        setTimeout(function() {
+            console.log(i);
+        }, i*1000);
+    }
+    console.log("printed before 1");
+} 
+
+z3();
