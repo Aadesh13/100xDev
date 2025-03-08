16e442fdf3563fc63c1401664ea7eae473895f9d
Aadesh Thakur <aadeshkbt@gmail.com>
2025-03-08 21:29:29 +0530

    http server-1

---
+++ http-server-1/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('<b> Hi there! </b>')
+  })
+
+app.get('/hello',(req, res) => {
+    res.send("Hello World!")
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.send("This is post request")
+    })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
