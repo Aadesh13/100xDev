513d08eb3a04b87265526d988c3d5368ff8aa7ec
Aadesh Thakur <aadeshkbt@gmail.com>
2025-08-27 01:07:56 +0530

    route & lazy loading

--- week7/reactRoute/src/App.jsx
+++ week7/reactRoute/src/App.jsx
@@ -1,13 +1,39 @@
-
+import {lazy, Suspense} from 'react'
+import { BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
 import './App.css'
 
+const Landing = lazy(() => import('./Components/Landing'))
+const Dashboard = lazy(() => import('./Components/Dashboard'))
+
 function App() {
-  
+
   return (
-    <>
-      
-    </>
+    <div>
+      <BrowserRouter>
+        <Appbar />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path='/Dashboard' element={<Dashboard />} />
+            <Route path='/' element={<Landing />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </div>
   )
+}
+
+function Appbar() {
+  const navigate = useNavigate();
+
+  return  <div>
+    <button onClick={() => {
+      navigate('/');
+    }}>Landing Page</button>
+
+    <button onClick={() => {
+      navigate('/Dashboard');
+    }}>Dashboard</button>
+  </div>
 }
 
 export default App
